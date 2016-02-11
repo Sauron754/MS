e5c9e2dcbf227f2e921ab41cd50472cae4aefaf1
Sauron754 <advancedcode754@gmail.com>
2016-02-11 17:17:45 +0100

    add modified version of MPS_modules for debugging

--- MPS_modules.py
+++ MPS_modules.py
@@ -1,4 +1,11 @@
 #linear movement module for x,y,z dimensions without inbound collisions and multiple objects
+#MPS Modules debug mode
+#
+#
+#
+#
+#
+#debug mode prints every variable which is beeing modified
 def lin_mov(object_number):
 	#!-- test global namespace system --!#
 	#print(test_namespaces)
@@ -7,6 +14,7 @@ def lin_mov(object_number):
 	from MPS import object_properties
 	from MPS import sequence_time
 	global object_properties
+	print("object_properties: ", object_properties)
 	if object_number < 5:
 		object_index = object_number * 17
 		x_pos = object_properties[0 + object_number]
@@ -16,15 +24,19 @@ def lin_mov(object_number):
 		ymov = object_properties[4 + object_number]
 		zmov = object_properties[5 + object_number]
 		coordinates = [x_pos, y_pos, z_pos]
+		print("initial coordinates: ", coordinates)
 		movement = [xmov, ymov, zmov]
+		print("initial movement:", movement)
 		sequence_movement = [False, False, False]
 		#---!sets movement per calculation process according to precision!---#
 		for i in range(3):
 			sequence_movement[i] = movement[i] * sequence_time
+		print("sequence_movement: ", sequence_movement)
 		new_x_pos = x_pos + sequence_movement[0]
 		new_y_pos = y_pos + sequence_movement[1]
 		new_z_pos = z_pos + sequence_movement[2]
 		new_coordinates = [new_x_pos, new_y_pos, new_z_pos]
+		print("new_coordinates", new_coordinates)
 		return new_coordinates
 	else:
 		#!-- OSError is just a placeholder for the upcoming selfdefined error --!#
