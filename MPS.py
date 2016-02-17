393e8ff1cc6b071fbeebdbc06cdf12cf8942b5a3
Sauron754 <advancedcode@gmail.com>
2016-02-17 22:09:44 +0100

    work on setting up base

--- MPS.py
+++ MPS.py
@@ -8,3 +8,14 @@
 #
 #
 #
+def MPS(precision, time_total, object_1_posx, object_1_posy, object_1_posz, object_1_xmov, object_1_ymov, object_1_zmov):
+	import MPS_object
+	import MPS_modules
+	import decimal 
+	#creates needed presets for MPS to work
+	decimal.getcontext().prec = 28
+	assigned_objects = 0
+	negative_precision = 0 - precision
+	sequence_time = 10**negative_precision
+	#!
+	object_1 = object_class(object_1_posx_dec, object_1_posy_dec, object_1_posz_dec, object_1_xmov_dec, object_1_ymov_dec, object_1_zmov_dec)
--- MPS_modules.py
+++ MPS_modules.py
@@ -8,3 +8,8 @@
 #
 #
 #
+def lin_mov(object_number):
+	import MPS #might cause recursive error!
+	if object_number == 1:
+		current_object = MPS.object_1
+	#more loops for more objects will be added in the future
